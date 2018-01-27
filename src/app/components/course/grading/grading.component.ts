9a265f0a68eb9a0cfabed283350be1824083bd96
Felix Kalley <felix.kalley@gmail.com>
2018-01-27 04:14:41 +0100

    change ui elements
    add color
    add table
    add radio buttons
    add json in controller

--- src/app/components/course/grading/grading.component.ts
+++ src/app/components/course/grading/grading.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradingComponent implements OnInit {
   title = `Notenverwaltung ASE WS17/18 !`;
-schemeCreateMode = false;
+  schemeCreateMode = false;
   schemeEdit = false;
 
   
@@ -15,13 +15,64 @@ schemeCreateMode = false;
 
   ngOnInit() {
   
-  //const json = require("../../../assets/data/schema_basic.json");
+      var json = {
+        "teilnehmer": [
+            {
+                "id": 1,
+                "mtknr": 1655429,
+                "name": "Guder",
+                "vorname": "Markus",
+                "studiengang": "M.Sc. Medieninfo (1)",
+                "fachsemester": 1,
+                "mail": "markus.guder@gmail.com",
+                "status": "ZU"
+            }
+        ],
+        "bewertungsschema": {
+            "allgemeine_infos": {
+                "notenschluessel": [
+                    ["note", "obergrenze", "untergrenze"],
+                    ["note", "obergrenze", "untergrenze"],
+                    ["note", "obergrenze", "untergrenze"]
+
+                ],
+                "bewertungseinheit": ""
+            },
+            "aufgaben": [
+                {
+                    "id": 0,
+                    "position": 0,
+                    "gewichtung": 1.0,
+                    "max_punkt": 0,
+                    "comment_public": true,
+                    "comment_privat": true,
+                    "beschreibung": "its bad!",
+                    "bewertungs_hinweis": "its really bad!"
+                }
+            ]
+        },
+        "bewertung": [
+            {
+                "student_id": 1,
+                "aufgaben": [
+                    {
+                        "aufgaben_id": 0,
+                        "erreichte_punkte": 0,
+                        "comment_privat": "",
+                        "comment_public": ""
+                    }
+                ]
+            }
+        ]
+    }
+
   
-  //console.log(json);
-  console.log(this.schemeCreateMode);
-  
+    console.log(json);
+    console.log(this.schemeCreateMode);
+
   
   }
+  
   setCreateMode(new_status): void{
   this.schemeCreateMode = new_status;
   console.log(this.schemeCreateMode);
