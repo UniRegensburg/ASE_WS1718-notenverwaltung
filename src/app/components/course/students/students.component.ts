e436671edcab1662ea476696e134046563bb02bb
Ewald Reinhardt <ewald.reinhardt.1@gmail.com>
2018-01-28 12:57:47 +0100

    add open file #35

--- src/app/components/course/students/students.component.ts
+++ src/app/components/course/students/students.component.ts
@@ -12,5 +12,37 @@ export class StudentsComponent implements OnInit {
 
   ngOnInit() {
   }
+  // function openDialog(){
+    // console.log("yo")
+  // }
+  openDialog(): void{
+    console.log("Yo")
+    var app = require('electron').remote;
+    var dialog = app.dialog
+    var fs = require('fs')
+    dialog.showOpenDialog((fileNames) =>{
+      if (fileNames === undefined){
+        console.log("No file selected")
+        return;
+      }
+
+      // console.log(fileNames)
+
+      fs.readFile(fileNames[0], 'utf-8', (err, data) => {
+        if(err){
+          alert("An error ocurred reading the file: "+err.message);
+          return;
+        }
+        this.processData(data)
+        // console.log("The file content is:"+data);
+      })
+    });
+    }
+    processData(data): void{
+      console.log("DATA:"+data)
+
+    }
+
+
 
 }
