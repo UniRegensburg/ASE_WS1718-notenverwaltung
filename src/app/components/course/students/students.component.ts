bdab9373256d0f7a97d1ef18cd2716e719f78b4d
Ewald Reinhardt <ewald.reinhardt.1@gmail.com>
2018-01-28 14:08:33 +0100

    add xls to data conversion #35

--- src/app/components/course/students/students.component.ts
+++ src/app/components/course/students/students.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as XLSX from 'ts-xlsx'
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
@@ -12,11 +12,8 @@ export class StudentsComponent implements OnInit {
 
   ngOnInit() {
   }
-  // function openDialog(){
-    // console.log("yo")
-  // }
+
   openDialog(): void{
-    console.log("Yo")
     var app = require('electron').remote;
     var dialog = app.dialog
     var fs = require('fs')
@@ -25,21 +22,79 @@ export class StudentsComponent implements OnInit {
         console.log("No file selected")
         return;
       }
-
-      // console.log(fileNames)
-
-      fs.readFile(fileNames[0], 'utf-8', (err, data) => {
-        if(err){
-          alert("An error ocurred reading the file: "+err.message);
-          return;
-        }
-        this.processData(data)
-        // console.log("The file content is:"+data);
-      })
+      this.processData(fileNames[0])
     });
     }
-    processData(data): void{
-      console.log("DATA:"+data)
+
+    processData(filepath): void{
+      // Get the correct sheet
+      var workbook = XLSX.readFile(filepath)
+      var sheetname = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[sheetname];
+
+      // Check where the table starts
+      var row = 0
+      var cell = 1
+      var address = String.fromCharCode(65+row)+cell
+      while(worksheet[address].v !="Nr"){
+        cell+=1
+        if(cell=== 20){
+          cell = 1
+          row+=1
+        }
+        address= String.fromCharCode(65+row)+cell;
+      }
+      // Move to the first row
+      cell+=1
+      address=String.fromCharCode(65+row)+cell;
+      // as long as there are numbers (= students)
+      var students = []
+      while(worksheet[address].v !==""){
+        var student = {
+            "id" : 0,
+            "mtknr" : 0,
+            "name" : 0,
+            "vorname" : 0,
+            "studiengang" : 0,
+            "fachsemester" : 0,
+            "mail" : 0,
+            "status" : 0
+        }
+        // move through the cells, saving data accordingly
+        for(var i=0;i<8;i++){
+          address= String.fromCharCode(65+row+i)+cell;
+          switch(i){
+            case 0:
+              student.id = worksheet[address].v
+              break;
+            case 1:
+              student.mtknr = worksheet[address].v
+              break;
+            case 2:
+              student.name  = worksheet[address].v
+              break;
+            case 3:
+              student.vorname = worksheet[address].v
+              break;
+            case 4:
+              student.studiengang = worksheet[address].v
+              break;
+            case 5:
+              student.fachsemester = worksheet[address].v
+              break;
+            case 6:
+              student.mail = worksheet[address].v
+              break;
+            case 7:
+              student.status = worksheet[address].v
+              break;
+          }
+        }
+        students.push(student)
+        cell+=1
+        address= String.fromCharCode(65+row)+cell;
+      }
+      console.log(students)
 
     }
 
