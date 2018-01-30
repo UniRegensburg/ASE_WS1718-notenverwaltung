683e2e2a7027155486e3543a788683de4e7a25b6
Gina Maria Wolf <ginamariawolf@gmail.com>
2018-01-31 00:28:01 +0100

    first diagramm in results #13

--- src/app/components/course/results/results.component.ts
+++ src/app/components/course/results/results.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalDataService, ExportService } from '../../../providers/index';
 
+declare var $: any
+
+var chartJs = require('chart.js');
+
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
@@ -9,9 +14,49 @@ import { GlobalDataService, ExportService } from '../../../providers/index';
 export class ResultsComponent implements OnInit {
   title = `Notenverwaltung ASE WS17/18 !`;
 
-  constructor(public exportService: ExportService) { }
+  private current_project: any;
+  private current_project_name: String;
+  private participants: Array<any>;
+  private display_user_list: boolean = false;
+  private user_grading_list: any;
+
+  private barChart: any;
+
+  constructor(public exportService: ExportService) { 
+
+  }
 
   ngOnInit() {
+    this.initGraphView();
+  }
+
+  initGraphView(): void{
+    this.initBarChart();
+  }
+
+  initBarChart():void {
+
+    this.barChart = new chartJs(document.getElementById("bar-chart"), {
+      type: 'bar',
+      data: {
+        labels: ["1.0", "1.3", "1.7", "2.0", "2.3", "2.7", "3.0", "3.3", "3.7", "4.0", "5.0"],
+        datasets: [
+          {
+            //label: "Population (millions)",
+            backgroundColor: ["#c2185b", "#ad1457","#880e4f","#d81b60","#c2185b", "#ad1457","#880e4f","#d81b60","#c2185b", "#ad1457","#880e4f","#d81b60",],
+            data: [1,3,7,7,2,7,3,2,1,10,4,0]
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: false,
+          text: 'Notenspiegel'
+        }
+      }
+  });
+
 
   }
 
