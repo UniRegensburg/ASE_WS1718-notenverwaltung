5bdb11d19904ae9712976f00150950cad430db77
Gina Maria Wolf <ginamariawolf@gmail.com>
2018-01-31 01:11:34 +0100

    further diagrams #13

--- src/app/components/course/results/results.component.ts
+++ src/app/components/course/results/results.component.ts
@@ -21,8 +21,10 @@ export class ResultsComponent implements OnInit {
   private user_grading_list: any;
 
   private barChart: any;
+  private polarChart: any;
+  private scatterChart: any;
 
-  constructor(public exportService: ExportService) { 
+  constructor(public exportService: ExportService) {
 
   }
 
@@ -30,12 +32,13 @@ export class ResultsComponent implements OnInit {
     this.initGraphView();
   }
 
-  initGraphView(): void{
+  initGraphView(): void {
     this.initBarChart();
+    this.initPolarChart();
+    this.initScatterChart();
   }
 
-  initBarChart():void {
-
+  initBarChart(): void {
     this.barChart = new chartJs(document.getElementById("bar-chart"), {
       type: 'bar',
       data: {
@@ -43,8 +46,8 @@ export class ResultsComponent implements OnInit {
         datasets: [
           {
             //label: "Population (millions)",
-            backgroundColor: ["#c2185b", "#ad1457","#880e4f","#d81b60","#c2185b", "#ad1457","#880e4f","#d81b60","#c2185b", "#ad1457","#880e4f","#d81b60",],
-            data: [1,3,7,7,2,7,3,2,1,10,4,0]
+            backgroundColor: ["#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60",],
+            data: [1, 3, 7, 7, 2, 7, 3, 2, 1, 10, 4, 0]
           }
         ]
       },
@@ -55,9 +58,149 @@ export class ResultsComponent implements OnInit {
           text: 'Notenspiegel'
         }
       }
-  });
+    });
+  }
 
+  initPolarChart(): void {
+    this.polarChart = new chartJs(document.getElementById("polar-chart"), {
+      type: 'polarArea',
+      data: {
+        labels: ["StA1", "StA2", "StA3", "StA4", "Abschlussprojekt"],
+        datasets: [
+          {
+            // label: "Population (millions)",
+            backgroundColor: ["#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60",],
+            data: [40, 90, 85, 60, 70]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: false,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+    });
+  }
 
+  initScatterChart(): void {
+    this.scatterChart = new chartJs(document.getElementById("scatter-chart"), {
+      type: 'bubble',
+
+      data: {
+        datasets: [{
+          backgroundColor: ["#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60", "#c2185b", "#ad1457", "#880e4f", "#d81b60"],
+          borderColor: "transparent",
+
+          label: 'Anzahl Studierende',
+          data: [{
+            x: 0,
+            y: 0
+          }, {
+            x: 1,
+            y: 40,
+            r: 10
+          }, {
+            x: 1,
+            y: 80,
+            r: 10
+          }, {
+            x: 1,
+            y: 100,
+            r: 1
+          }, {
+            x: 1,
+            y: 70,
+            r: 2
+          }, {
+            x: 1,
+            y: 10,
+            r: 4
+          }, {
+            x: 2,
+            y: 60,
+            r: 5
+          }, {
+            x: 2,
+            y: 80,
+            r: 7
+          }, {
+            x: 2,
+            y: 90,
+            r: 2
+          }, {
+            x: 2,
+            y: 95,
+            r: 10
+          }, {
+            x: 2,
+            y: 70,
+            r: 3
+          }, {
+            x: 2,
+            y: 60,
+            r: 7
+          }, {
+            x: 3,
+            y: 50,
+            r: 5
+          }, {
+            x: 4,
+            y: 40,
+            r: 10
+          }, {
+            x: 4,
+            y: 80,
+            r: 10
+          }, {
+            x: 5,
+            y: 100,
+            r: 1
+          }, {
+            x: 5,
+            y: 70,
+            r: 2
+          }, {
+            x: 5,
+            y: 10,
+            r: 4
+          }, {
+            x: 5,
+            y: 60,
+            r: 5
+          }, {
+            x: 4,
+            y: 80,
+            r: 7
+          }, {
+            x: 2,
+            y: 90,
+            r: 2
+          }, {
+            x: 2,
+            y: 95,
+            r: 10
+          }, {
+            x: 3,
+            y: 70,
+            r: 3
+          }, {
+            x: 3,
+            y: 80,
+            r: 2
+          }]
+        }]
+      },
+      options: {
+        //TODO: make following work https://dima117.github.io/Chart.Scatter/
+        scaleShowGridLines: false,
+        xScaleOverride: true,
+        title: {
+          display: false,
+          text: 'Predicted world population (millions) in 2050'
+        },
+      }
+    });
   }
 
   export(string): void {
