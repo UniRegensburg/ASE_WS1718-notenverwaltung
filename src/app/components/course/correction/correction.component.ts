4d6650e8dcc73261eeb9987f2214c638ef25de6f
Markus Guder <progamer101101@gmx.de>
2018-01-31 10:20:50 +0100

    add grading logic #27

--- src/app/components/course/correction/correction.component.ts
+++ src/app/components/course/correction/correction.component.ts
@@ -19,10 +19,29 @@ export class CorrectionComponent implements OnInit {
 
   private current_project: any;
   private current_task: any;
+
   private tasks: Array<any>;
-  private participants: Array<any>;
-  private correctionMode: boolean;
-  private old_window_state;
+  private students: Array<any>;
+  private grading: Array<any>;
+
+  private correction_mode: string = "student";  //task
+  private current_correction: any;
+  private screen_mode: boolean;
+  private old_window_state:any;
+  private task_counter: number;
+
+  /**
+  * 
+  {
+    "student_id": 1,
+    "einzelwertungen": [{
+      "aufgaben_id": 0,
+      "erreichte_punkte": 0,
+      "comment_privat": "",
+      "comment_public": ""
+    }]
+  }
+  */
   
   constructor(public dataService: GlobalDataService) { 
     
@@ -31,23 +50,54 @@ export class CorrectionComponent implements OnInit {
   ngOnInit() {
     this.dataService.getCurrentProject().subscribe(current_project => {      
       this.current_project = current_project;
+      
       this.tasks = this.current_project.bewertungsschema.aufgaben;
-      this.current_task = this.tasks[0];
-      //this.current_task = this.tasks[0];
-      //console.log(this.current_task.beschreibung);      
+      this.students = this.current_project.teilnehmer;
+      this.grading = this.current_project.bewertung;
+
+      this.setCurrentTask();             
     });
   }
 
-  setCorrectionMode():void {    
-    this.correctionMode = !this.correctionMode;
+  setScreenMode():void {    
+    this.screen_mode = !this.screen_mode;
     let win = remote.getCurrentWindow();    
 
-    if(this.correctionMode){
+    if(this.screen_mode){
       this.old_window_state = win.getBounds();
       win.setBounds({x: 240, y: 192, width: 440, height: 600});
     }
     else{
       win.setBounds(this.old_window_state);
+    }
+  }
+
+  setCorretionMode(value): void{
+    this.correction_mode = value;
+  }
+
+  setCurrentTask(): void{
+    console.log(this.current_correction);
+    
+    if(!this.task_counter) this.task_counter = 0
+    else this.task_counter++;
+
+    this.current_task = this.tasks[this.task_counter];
+    this.current_correction = {
+      "aufgaben_id": this.current_task.id,
+      "erreichte_punkte": 0,
+      "comment_privat": "",
+      "comment_public": ""
+    }
+  }
+
+  getCurrentTask(): any{
+    if(this.correction_mode === "student"){
+      let single_grade = this.grading[this.grading.length-1].einzelwertungen;
+      return single_grade[single_grade.length-1];
+    }
+    else{
+      return ""
     }
   }
 }
