3bdbaac04e7d23595a9a5afceaa7642752a09154
Gina Maria Wolf <ginamariawolf@gmail.com>
2018-03-06 13:58:24 +0100

    saving new course works with one button #82

--- src/app/components/home/newcourse/newcourse.component.ts
+++ src/app/components/home/newcourse/newcourse.component.ts
@@ -19,64 +19,61 @@ export class NewCourseComponent implements OnInit {
     "title": "",
     "path": ""
   };
+
+  //TODO: an zentrale Stelle:
+  private basic_schema = {
+    "teilnehmer": [],
+    "bewertungsschema": {},
+    "bewertung": []
+  };
+
+  //TODO: was ist das und was tut das?
   private view_mode: boolean = true;
 
-  constructor(
-    public dataService: GlobalDataService,
-    public router: Router
-  ) { }
+  constructor(public dataService: GlobalDataService, public router: Router) { }
 
-  ngOnInit() {
-    //this.course_oject = new File();
-  }
-
-  store(file) {
-    this.course_oject.path = file;
-    //TODO: schöner machen
-    alert("Speicherort festgelegt: " + file)
-  }
-
-  chooseFolder() {
-    var app = require('electron').remote;
-    var dialog = app.dialog
-
-    dialog.showOpenDialog({ properties: ['openDirectory'] }, (fileNames) => {
-      if (fileNames === undefined) {
-        console.log("No file selected")
-        return;
-      }
-      this.store(fileNames[0])
-    });
-  }
+  ngOnInit() { }
 
   createCourse(): void {
     this.course_oject.file_name = this.course_oject.title + ".json";
-    console.log(this.course_oject.path)
+    var app = require('electron').remote;
+    var dialog = app.dialog
 
-    if (this.course_oject.path) {
-
-      let filePath = this.course_oject.path + "/" + this.course_oject.title + ".json";
-      let basic_schema = {
-        "teilnehmer": [],
-        "bewertungsschema": {},
-        "bewertung": []
-      };
-
-      writeFile(filePath, JSON.stringify(basic_schema), (err) => {
-        if (err) {
-          alert("An error ocurred creating the file " + err.message);
+    var chooseFolder = new Promise((resolve, reject) => {
+      dialog.showOpenDialog({ properties: ['openDirectory'] }, (fileNames) => {
+        if (fileNames === undefined) {
+          //TODO: muss der Nutzer das auch sehen können?
+          console.log("No filename selected")
+          reject("No filename seleeeeected")
+          return;
         }
-        else {
-          alert("The file has been succesfully saved");
-          this.dataService.getLocalFile(filePath).subscribe(
-            data => {
-              this.router.navigate(['course/overview']);
-            });
-        }
+        this.course_oject.path = fileNames[0];
+        console.log("Speicherort festgelegt.");
+        resolve();
       });
-    } else {
-      alert("Please select a directory!")
-    }
+    });
+
+    chooseFolder.then(() => {
+      if (this.course_oject.path) {
+        let filePath = this.course_oject.path + "/" + this.course_oject.title + ".json";
+       
+        writeFile(filePath, JSON.stringify(this.basic_schema), (err) => {
+          if (err) {
+            alert("An error ocurred creating the file " + err.message);
+          }
+          else {
+            alert("The file has been succesfully saved");
+            this.dataService.getLocalFile(filePath).subscribe(
+              data => {
+                this.router.navigate(['course/overview']);
+              });
+          }
+        });
+      } else {
+        //TODO: sichtbar machen?
+        alert("Please select a directory!")
+      }
+    });
   }
 
 }
