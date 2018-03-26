83f7292a125cc15523210ae3191ac500bb2fe484
Ewald Reinhardt <ewald.reinhardt.1@gmail.com>
2018-03-26 13:31:00 +0200

    fix first line of export #38

--- src/app/providers/lsf-export.service.ts
+++ src/app/providers/lsf-export.service.ts
@@ -13,6 +13,7 @@ export class lsfExportService{
     private filePath: any;
     private current_project_results: any;
     private current_project_students: any;
+    private current_project_grading: any;
 
     constructor(private dataService: GlobalDataService) {}
 
@@ -21,17 +22,20 @@ export class lsfExportService{
         this.dataService.getCurrentProject().subscribe(current_project =>{
             this.current_project = current_project;
             this.current_project_name = this.current_project.title;
-            this.current_project_students = this.current_project.teilnehmer
-            this.current_project_results = this.current_project.bewertung
+            this.current_project_students = this.current_project.teilnehmer;
+            this.current_project_results = this.current_project.bewertung;
+            this.current_project_grading = this.current_project.bewertungsschema;
             this.filePath = this.dataService.getFilePath().substring(0,this.dataService.getFilePath().lastIndexOf("\\")+1)+ "lsf_export.csv";
-            console.log(this.filePath)
             //  https://stackoverflow.com/questions/20279484/how-to-access-the-correct-this-inside-a-callback
             var self = this;
             var stream = fs.createWriteStream(this.filePath);
             stream.once('open', function(fd){
                 // TODO: aufgabennummer variabel machen
-                stream.write("StudentName,StudentVorname,Matrikelnummer,Aufgabe1,Aufgabe2,Aufgabe3,Aufgabe4\n");
-
+                stream.write("StudentName,StudentVorname,Matrikelnummer");
+                for (let aufgabe of self.current_project_grading.aufgaben){
+                    stream.write(",Aufgabe"+aufgabe.id)
+                }
+                stream.write("\n")
                 for (let result of self.current_project_results){
                     for (let student of self.current_project_students){
                         if (result.student_id === student.id){
