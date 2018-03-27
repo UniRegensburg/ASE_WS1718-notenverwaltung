4bd8fd3fed0d073f797306d9f5e61ad9103e2cd2
marc101101 <progamer101101@gmx.de>
2018-03-27 13:54:13 +0200

    add pipe function for search

---
+++ src/app/pipes/search-student.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'searchStudent'})
+export class SearchStudentPipe implements PipeTransform {
+
+  private fitSearch(item: string, field:string): boolean{
+    let returnVal = false;
+    console.log(item);
+    if(item.includes(field)){
+      returnVal = true;
+    }
+    return returnVal;
+  }
+
+  transform(items: any[], field: string): any[] {   
+   if (!field || field.trim() === '') return items;
+   return items.filter(item => {
+     return this.fitSearch(JSON.stringify(item).toLowerCase(), field.toLowerCase());
+   });
+  }
+}
